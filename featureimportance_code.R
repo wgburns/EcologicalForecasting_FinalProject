78dc65f667aaa211b6e05a035d3d066e7937c2b0
KenyanGeek <hchege@uvm.edu>
2019-11-18 21:08:36 -0500

    added feature importance R code

---
+++ featureimportance_code.R
@@ -0,0 +1,94 @@
+
+#Combined feature importance abd Time series forecasting 
+# I got this information from here:
+
+#https://rpubs.com/mattBrown88/TimeSeriesMachineLearning
+
+# libraries we're going to use
+library(xgboost) # for xgboost
+library(tidyverse) # general utility functions
+library(data.table)
+library(dplyr)
+library(Matrix)
+library(zoo)
+
+#Here i read in the data
+data <- read.csv("/Users/chege/Desktop/quantitative reasoning final project/HF_stAB18_Clean.csv")
+
+#Here i check how my data looks
+head(data)
+
+#here i check the format of my variables
+str(data)
+
+
+# get training labels i.e our bloom variable
+bloom_variable <-data%>%
+  select(PC_RFU_0.5m) # get the column with the bloom measurements
+
+
+# check out the first few lines
+head(bloom_variable) # of our target variable
+
+#Just have a dataframe with the target variable removed
+
+without_bloom_indicator <- data %>%
+  select(-starts_with("PC"))
+
+#Here i remove redundant columns and keep away whatever is not numeric
+
+without_bloom_numeric <- without_bloom_indicator %>%
+  #select(-ID) %>% # the case id shouldn't contain useful information
+  select(-c(PH_0.5m,ID,Turb_NTU_0.5m,  TIMESTAMP,CHL_RFU_0.5m, deltaDO)) %>% # Here i have removed Ph and timestamp
+  select_if(is.numeric) # select remaining numeric columns
+
+# make sure that our dataframe is all numeric
+str(without_bloom_numeric)
+
+
+#convert the dataframe into a matrix
+without_bloom_matrix <- data.matrix(without_bloom_numeric)
+
+
+
+# get the numb 70/30 training test split
+
+numberOfTrainingSamples <- round(nrow(bloom_variable)*.7)
+
+# training data
+train_data <- without_bloom_matrix[1:numberOfTrainingSamples,]
+train_labels <- bloom_variable[1:numberOfTrainingSamples,]
+
+# testing data
+test_data <- without_bloom_matrix[-(1:numberOfTrainingSamples),]
+test_labels <- bloom_variable[-(1:numberOfTrainingSamples),]
+
+
+# put our testing & training data into two seperates Dmatrixs objects
+dtrain <- xgb.DMatrix(data = train_data, label= train_labels)
+dtest <- xgb.DMatrix(data = test_data, label= test_labels)
+
+# train a model using our training data
+model <- xgboost(data = dtrain, # the data   
+                 nround = 2, # max number of boosting iterations
+                 objective = "reg:linear")  
+                 # the objective function
+
+# generate predictions for our held-out testing data
+pred <- predict(model, dtest)
+
+xgb.ggplot.importance(importance_matrix = importance)
+
+
+# get information on how important each feature is
+importance <- xgb.importance(names(without_bloom_matrix), model = model)
+
+#Here is code to generate cute graphs for publication
+tiff("feature_importances_HF_stAB18_Clean.tiff", units="in", width=5, height=5, res=300)
+
+xgb.plot.importance(importance)
+
+
+dev.off()
+# and your plot is in your files as feature_importances.tiff and publication ready!
+
